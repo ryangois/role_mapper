5087e3b46eebba894ed547559ff8a14189bb2933
ryangois <ryangoisdev@gmail.com>
2023-05-16 19:29:36 -0300

    select implementation

---
+++ src/components/Select/index.js
@@ -0,0 +1,14 @@
+import './Select.css'
+
+const Select = (props) => {
+    return (
+        <div className='select'>
+            <label>{props.label}</label>
+            <select>
+                {props.itens.map(item => <option key={item}>{item}</option>)}
+            </select>
+        </div>
+    )
+}
+
+export default Select
