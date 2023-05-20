4adb2fa067f0346c624c0d243ef144353c83eb4f
ryangois <ryangoisdev@gmail.com>
2023-05-20 19:12:27 -0300

    user interaction

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+import { useState } from 'react';
 import Banner from './components/Banner';
 import Form from './components/Form';
 
 function App() {
+
+  const [colaborators, setColaborator] = useState([])
+
+  const onNewColaborator = (colaborator) => {
+    console.log(colaborator);
+    setColaborator([...colaborators, colaborator])
+  }
+
   return (
     <div className="App">
       <Banner />
-      <Form />
+      <Form onSubmited={colaborator => onNewColaborator(colaborator)} />
     </div>
   );
 }
