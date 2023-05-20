0e6a71b81166e9401fdce0e8a803fff2d3fd130a
ryangois <ryangoisdev@gmail.com>
2023-05-19 22:05:06 -0300

    state control for text fields

--- src/components/TextField/index.js
+++ src/components/TextField/index.js
@@ -1,11 +1,17 @@
 
+import { useState } from 'react'
 import './TextField.css'
 
 const TextField = (props) => {
+
+    const onTyped = (event) => {
+        props.onChanged(event.target.value)
+    }
+
     return (
         <div className="text-field">
             <label>{props.label}</label>
-            <input placeholder={props.placeholder}/>
+            <input value ={props.value} onChange={onTyped} required placeholder={props.placeholder}/>
         </div>
     )
 }
