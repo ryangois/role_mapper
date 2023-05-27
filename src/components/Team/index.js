20ca68d0afef600e4f79ac2dacaa8d91a0a14b1c
ryangois <ryangoisdev@gmail.com>
2023-05-26 21:12:27 -0300

    colaborators card implementation on team section

--- src/components/Team/index.js
+++ src/components/Team/index.js
@@ -1,9 +1,11 @@
+import Colaborator from '../Colaborator'
 import './Team.css'
 
 const Team = (props) => {
     return (
         <section className='team' style={{ backgroundColor: props.secondaryColor }}>
             <h3 style={{ borderColor: props.primaryColor }}>{props.name}</h3>
+            <Colaborator />
         </section>
     )
 }
