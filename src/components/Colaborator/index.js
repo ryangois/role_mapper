dab027d397de50a449c3eaea3c32f4c951bae70a
ryangois <ryangoisdev@gmail.com>
2023-05-26 21:12:11 -0300

    colaborators static component

---
+++ src/components/Colaborator/index.js
@@ -0,0 +1,17 @@
+import './Colaborator.css'
+
+const Colaborator = () => {
+    return (
+        <div className='colaborator'>
+            <div className='header'>
+                <img src='https://avatars.githubusercontent.com/u/122648441?v=4' alt='Ryan Gois' />
+            </div>
+            <div className='footer'>
+                <h4>Ryan Gois</h4>
+                <h5>Software Developer</h5>
+            </div>
+        </div>
+    )
+}
+
+export default Colaborator
