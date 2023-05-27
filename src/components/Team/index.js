a413a03cfcf36504630230f52d69e8f1c282281a
ryangois <ryangoisdev@gmail.com>
2023-05-27 12:48:50 -0300

    card appearance based on team length

--- src/components/Team/index.js
+++ src/components/Team/index.js
@@ -3,7 +3,7 @@ import './Team.css'
 
 const Team = (props) => {
     return (
-        <section className='team' style={{ backgroundColor: props.secondaryColor }}>
+        props.colaborators.length > 0 && <section className='team' style={{ backgroundColor: props.secondaryColor }}>
             <h3 style={{ borderColor: props.primaryColor }}>{props.name}</h3>
             <div className='colaborators'>
                 {props.colaborators.map(colaborator => <Colaborator name={colaborator.name} role={colaborator.role} image={colaborator.image} />)}
