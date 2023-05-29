343970f62d4d66d79e77a2ba80db756376a91293
ryangois <ryangoisdev@gmail.com>
2023-05-28 21:51:32 -0300

    reset select implementation

--- src/components/Select/index.js
+++ src/components/Select/index.js
@@ -5,6 +5,7 @@ const Select = (props) => {
         <div className='select'>
             <label>{props.label}</label>
             <select onChange={event => props.onChanged(event.target.value)} value={props.value}>
+                <option value=""></option>
                 {props.itens.map(item => <option key={item}>{item}</option>)}
             </select>
         </div>
