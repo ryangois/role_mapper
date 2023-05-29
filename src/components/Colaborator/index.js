8ae9d78973ebb32ed7672f750cd7d76c349e1fd5
ryangois <ryangoisdev@gmail.com>
2023-05-29 13:57:53 -0300

    GitHub user variable

--- src/components/Colaborator/index.js
+++ src/components/Colaborator/index.js
@@ -1,12 +1,13 @@
 import './Colaborator.css'
 
 const Colaborator = ({ name, image, role, backgroundColor }) => {
+    let userGitHub = "https://github.com/" + image
     return (
-        <a className='card' href={image} target="_blank" rel="noopener noreferrer">
+        <a className='card' href={userGitHub} target="_blank" rel="noopener noreferrer">
             <div className='colaborator'>
                 <div className='header' style={{ backgroundColor: backgroundColor }}>
 
-                    <img src={"https://github.com/" + image + ".png"} alt='props.name' />
+                    <img src={userGitHub + ".png"} alt='props.name' />
                 </div>
                 <div className='footer'>
                     <h4>{name}</h4>
