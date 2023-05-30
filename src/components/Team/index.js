3338d6af986988dca2c2ffc7125d106708d6ab2d
ryangois <ryangoisdev@gmail.com>
2023-05-29 21:15:16 -0300

    onDelete implementation

--- src/components/Team/index.js
+++ src/components/Team/index.js
@@ -1,12 +1,14 @@
 import Colaborator from '../Colaborator'
 import './Team.css'
 
-const Team = (props) => {
+const Team = (props, onDelete) => {
     return (
         props.colaborators.length > 0 && <section className='team' style={{ backgroundColor: props.secondaryColor }}>
             <h3 style={{ borderColor: props.primaryColor }}>{props.name}</h3>
             <div className='colaborators'>
-                {props.colaborators.map(colaborator => <Colaborator backgroundColor={props.primaryColor} key={colaborator.name} name={colaborator.name} role={colaborator.role} image={colaborator.image} />)}
+                {props.colaborators.map(colaborator => {
+                    return <Colaborator backgroundColor={props.primaryColor} key={colaborator.name} name={colaborator.name} role={colaborator.role} image={colaborator.image} onDelete={onDelete} />
+                })}
             </div>
         </section>
     )
