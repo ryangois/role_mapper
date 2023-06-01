c68482f21ea4f86dac2945a4ccc3df88753138bd
ryangois <ryangoisdev@gmail.com>
2023-06-01 20:29:01 -0300

    color event handle

--- src/components/Team/index.js
+++ src/components/Team/index.js
@@ -1,10 +1,10 @@
 import Colaborator from '../Colaborator'
 import './team.css'
 
-const Team = ({ team, colaborators, onDelete }) => {
+const Team = ({ team, colaborators, onDelete, changeColor }) => {
     return (
         colaborators.length > 0 && <section className='team' style={{ backgroundImage: 'url(/images/fundo.png)', backgroundColor: team.primaryColor }}>
-            <input value={team.secondaryColor} type='color' className='color-input' />
+            <input onChange={event => changeColor(event.target.value, team.name)} value={team.secondaryColor} type='color' className='color-input' />
             <h3 style={{ borderColor: team.secondaryColor }}>{team.name}</h3>
             <div className='colaborators'>
                 {colaborators.map((colaborator, indice) => <Colaborator key={indice} colaborator={colaborator} corDeFundo={team.secondaryColor} onDelete={onDelete} />)}
