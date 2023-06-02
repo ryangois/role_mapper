8d04e3f61a3dbcbed6cff5bb727af0435139e150
ryangois <ryangoisdev@gmail.com>
2023-06-02 18:13:40 -0300

    creating ids with uuid API

--- src/App.js
+++ src/App.js
@@ -3,35 +3,43 @@ import Banner from "./components/Banner";
 import Form from "./components/Form";
 import Footer from "./components/Footer";
 import Team from "./components/Team";
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
 
   const [teams, setTeams] = useState([
     {
+      id: uuidv4(),
       name: 'Programação',
       color: '#57C278'
     },
     {
+      id: uuidv4(),
       name: 'Front-End',
       color: '#82CFFA'
     },
     {
+      id: uuidv4(),
       name: 'Data Science',
       color: '#A6D157'
     },
     {
+      id: uuidv4(),
       name: 'Devops',
       color: '#E06B69'
     },
     {
+      id: uuidv4(),
       name: 'UX e Design',
       color: '#DB6EBF'
     },
     {
+      id: uuidv4(),
       name: 'Mobile',
       color: '#FFBA05'
     },
     {
+      id: uuidv4(),
       name: 'Inovação e Gestão',
       color: '#FF8A29'
     },
@@ -39,114 +47,132 @@ function App() {
 
   const inicial = [
     {
-      name: 'Ryan Gois',
+      id: uuidv4(), name: 'Ryan Gois',
       role: 'Desenvolvedor de software',
       image: 'ryangois',
       team: teams[0].name
     },
     {
+      id: uuidv4(),
       name: 'GUILHERME LIMA',
       role: 'Desenvolvedor Python e JavaScript na Alura',
       image: 'guilhermeonrails',
       team: teams[0].name
     },
     {
+      id: uuidv4(),
       name: 'PAULO SILVEIRA',
       role: 'Hipster e CEO da Alura',
       image: 'peas',
       team: teams[0].name
     },
     {
+      id: uuidv4(),
       name: 'Ryan Gois',
       role: 'Desenvolvedor de software',
       image: 'ryangois',
       team: teams[1].name
     },
     {
+      id: uuidv4(),
       name: 'DANIEL ARTINE',
       role: 'Engenheiro de Software na Stone Age',
       image: 'danielartine',
       team: teams[1].name
     },
     {
+      id: uuidv4(),
       name: 'GUILHERME LIMA',
       role: 'Desenvolvedor Python e JavaScript na Alura',
       image: '	guilhermeonrails',
       team: teams[1].name
     },
     {
+      id: uuidv4(),
       name: 'PAULO SILVEIRA',
       role: 'Hipster e CEO da Alura',
       image: 'peas',
       team: teams[1].name
     },
     {
+      id: uuidv4(),
       name: 'Ryan Gois',
       role: 'Desenvolvedor de software',
       image: 'ryangois',
       team: teams[2].name
     },
     {
+      id: uuidv4(),
       name: 'GUILHERME LIMA',
       role: 'Desenvolvedor Python e JavaScript na Alura',
       image: '	guilhermeonrails',
       team: teams[2].name
     },
     {
+      id: uuidv4(),
       name: 'PAULO SILVEIRA',
       role: 'Hipster e CEO da Alura',
       image: 'peas',
       team: teams[2].name
     },
     {
+      id: uuidv4(),
       name: 'Ryan Gois',
       role: 'Desenvolvedor de software',
       image: 'ryangois',
       team: teams[3].name
     },
     {
+      id: uuidv4(),
       name: 'GUILHERME LIMA',
       role: 'Desenvolvedor Python e JavaScript na Alura',
       image: '	guilhermeonrails',
       team: teams[3].name
     },
     {
+      id: uuidv4(),
       name: 'PAULO SILVEIRA',
       role: 'Hipster e CEO da Alura',
       image: 'peas',
       team: teams[3].name
     },
     {
+      id: uuidv4(),
       name: 'Ryan Gois',
       role: 'Desenvolvedor de software',
       image: 'ryangois',
       team: teams[4].name
     },
     {
+      id: uuidv4(),
       name: 'GUILHERME LIMA',
       role: 'Desenvolvedor Python e JavaScript na Alura',
       image: '	guilhermeonrails',
       team: teams[4].name
     },
     {
+      id: uuidv4(),
       name: 'PAULO SILVEIRA',
       role: 'Hipster e CEO da Alura',
       image: 'peas',
       team: teams[4].name
     },
     {
+      id: uuidv4(),
       name: 'Ryan Gois',
       role: 'Desenvolvedor de software',
       image: 'ryangois',
       team: teams[5].name
     },
     {
+      id: uuidv4(),
       name: 'GUILHERME LIMA',
       role: 'Desenvolvedor Python e JavaScript na Alura',
       image: 'guilhermeonrails',
       team: teams[5].name
     },
     {
+      id: uuidv4(),
       name: 'PAULO SILVEIRA',
       role: 'Hipster e CEO da Alura',
       image: 'peas',
@@ -160,9 +186,9 @@ function App() {
     console.log('deletando colaborator');
   }
 
-  function teamColorChange(color, name) {
+  function teamColorChange(color, id) {
     setTeams(teams.map(team => {
-      if (team.name === name) {
+      if (team.id === id) {
         team.color = color;
       }
       return team;
