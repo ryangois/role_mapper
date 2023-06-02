7ca435a848e9b07a267bb3ebc71c30ddf5813b5f
ryangois <ryangoisdev@gmail.com>
2023-06-02 18:16:50 -0300

    colorChange function parameter change

--- src/components/Team/index.js
+++ src/components/Team/index.js
@@ -5,7 +5,7 @@ import hexToRgba from 'hex-to-rgba';
 const Team = ({ team, colaborators, onDelete, changeColor }) => {
     return (
         colaborators.length > 0 && <section className='team' style={{ backgroundImage: 'url(/images/fundo.png)', backgroundColor: hexToRgba(team.color, '0.6') }}>
-            <input onChange={event => changeColor(event.target.value, team.name)} value={team.color} type='color' className='color-input' />
+            <input onChange={event => changeColor(event.target.value, team.id)} value={team.color} type='color' className='color-input' />
             <h3 style={{ borderColor: team.color }}>{team.name}</h3>
             <div className='colaborators'>
                 {colaborators.map((colaborator, indice) => <Colaborator key={indice} colaborator={colaborator} corDeFundo={team.color} onDelete={onDelete} />)}
